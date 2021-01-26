9efe8a844e1e9efed46e5db83e9fec65ad17d9a8
Grant DeLong <ggoulae@gmail.com>
2021-01-26 10:44:24 -0600

    added sample stats analysis to notebook added setup.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,19 +1,27 @@
 import networkx as nx
 
-def get_careteam_data(care_team):
-    coefficients = nx.clustering(care_team.G)# Clustering coefficient of all nodes (in a dictionary)
-    # Average clustering coefficient with divide-by-zero check
-    avg_clust = sum(coefficients.values()) / len(coefficients) if len(coefficients) > 0 else 0  
-    experience = care_team.G.size(weight='weight') #Experience as sum of weights
-    team_edge_size = care_team.G.number_of_edges()
-    cumulative_experience = experience - team_edge_size
-    avg_cumulative_experience = cumulative_experience / len(care_team.care_team)#Average Cumulative Experience
+columns = ['discharge_id',
+        'avg_clust',
+        'cumulative_experience',
+        'avg_cumulative_experience',
+        'team_edge_size',
+        'team_size']
 
-    return {
-        'discharge_id': care_team.discharge_id,
-        'avg_clust': avg_clust,
-        'cumulative_experience': cumulative_experience,
-        'avg_cumulative_experience': avg_cumulative_experience,
-        "team_edge_size": team_edge_size,
-        "team_size": care_team.G.number_of_nodes()
-    }
+def get_careteam_data(care_team):
+    data = {}
+    data['discharge_id'] = care_team.discharge_id
+    
+    ''' Clustering coefficient of all nodes (in a dictionary) '''
+    clustering_coefficient = nx.clustering(care_team.G)
+    
+    ''' Average clustering coefficient with divide-by-zero check '''
+    data['avg_clust'] = sum(clustering_coefficient.values()) / len(clustering_coefficient) if len(clustering_coefficient) > 0 else 0 
+    
+    data['team_size'] = care_team.G.number_of_nodes()
+    data['team_edge_size'] = care_team.G.number_of_edges()
+    
+    experience = care_team.G.size(weight='weight') #Experience as sum of weights
+    data['cumulative_experience'] = experience - data['team_edge_size']
+    data['avg_cumulative_experience'] = data['cumulative_experience'] / len(care_team.care_team)#Average Cumulative Experience
+    
+    return data
